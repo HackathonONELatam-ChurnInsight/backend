b80d2c8f737b07b3a1bb53ae0e2aeaf599355b5c
evanghel1on <cfloresu@uni.pe>
2026-01-24 09:56:38 -0500

    config swagger backend url

--- src/main/java/com/one/hackathonlatam/dic25equipo69/churninsight/config/OpenApiConfig.java
+++ src/main/java/com/one/hackathonlatam/dic25equipo69/churninsight/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.one.hackathonlatam.dic25equipo69.churninsight.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -12,11 +13,14 @@ import java.util.List;
 @Profile("prod")
 public class OpenApiConfig {
 
+    @Value("${backend.base-url}")
+    private String url;
+
     @org.springframework.context.annotation.Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         Server prodServer = new Server()
-                .url("http://localhost:8080/api/v1")
+                .url(url)
                 .description("Local PROD");
         return new OpenAPI()
                 .info(new Info()
